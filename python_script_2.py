54927576537ccde6c8149f6d88b3e06e116438cf
KEVINMSI\Kevin <kevin.schuddinck.v6468@student.hogent.be>
2016-12-08 14:56:04 +0100

    no message

--- python_script_2.py
+++ python_script_2.py
@@ -8,12 +8,15 @@ grovepi.pinMode(sound_sensor, "INPUT")
 buzzer = 8
 grovepi.pinMode(buzzer, "OUTPUT")
 
+threshold_value = 55
+
 while True:
 	try:
 		temp = grovepi.analogRead(sound_sensor)
 		sensor_value = (20 * math.log10(temp + 1) )
 		print(sensor_value)
-		if sensor_value > 40 :
+		if sensor_value > threshold_value :
+			print('ALARM!')
 			grovepi.digitalWrite(buzzer,1)
 			time.sleep(1)
 			grovepi.digitalWrite(buzzer,0)
