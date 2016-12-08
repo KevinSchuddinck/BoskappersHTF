45415bf0010905503ef0086ea16905b6651bc2dc
KEVINMSI\Kevin <kevin.schuddinck.v6468@student.hogent.be>
2016-12-08 15:27:04 +0100

    no message

---
+++ python_script_1B.py
@@ -0,0 +1,32 @@
+import time
+import grovepi
+import json
+import requests
+
+button = 3
+grovepi.pinMode(button, "INPUT")
+
+buzzer = 8
+grovepi.pinMode(buzzer, "OUTPUT")
+
+url = 'http://192.168.50.148:4000/api'
+headers = {"team", "sensor"}
+
+while True:
+    try:
+        if grovepi.digitalRead(button):
+			data = {
+						team: { id: [3] }
+						sensor: {
+							id: [0]
+							state: [true]
+							value: [1]
+							}
+					})
+			r = requests.post(url, headers, stats = json.dumps(data))
+			grovepi.digitalWrite(buzzer,1)
+			time.sleep(1)
+			grovepi.digitalWrite(buzzer,0)
+		
+    except IOError:
+        print ("Error")
