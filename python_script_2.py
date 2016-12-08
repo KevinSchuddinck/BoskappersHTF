655a48103a0cbecda51d6d7e8fa275230b4c336f
KEVINMSI\Kevin <kevin.schuddinck.v6468@student.hogent.be>
2016-12-08 14:44:11 +0100

    update opdracht 2

--- python_script_2.py
+++ python_script_2.py
@@ -1,5 +1,6 @@
 import time
 import grovepi
+import math
 
 sound_sensor = 0
 
@@ -8,7 +9,7 @@ grovepi.pinMode(sound_sensor, "INPUT")
 while True:
 	try:
 		temp = grovepi.analogRead(sound_sensor)
-		sensor_value = (20 * log10(temp + 1) )
+		sensor_value = (20 * math.log10(temp + 1) )
 		print(sensor_value)
 		time.sleep(2)
 	except IOError:
