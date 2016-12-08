6dddaa48dc8159f0dc728355135e65ffe4ffd980
KEVINMSI\Kevin <kevin.schuddinck.v6468@student.hogent.be>
2016-12-08 14:48:14 +0100

    no message

--- python_script_2.py
+++ python_script_2.py
@@ -3,14 +3,21 @@ import grovepi
 import math
 
 sound_sensor = 0
-
 grovepi.pinMode(sound_sensor, "INPUT")
+
+buzzer = 8
+grovepi.pinMode(buzzer, "OUTPUT")
 
 while True:
 	try:
 		temp = grovepi.analogRead(sound_sensor)
 		sensor_value = (20 * math.log10(temp + 1) )
 		print(sensor_value)
+		if sensor_value > 40 :
+			grovepi.digitalWrite(buzzer,1)
+			time.sleep(1)
+			grovepi.digitalWrite(buzzer,0)
+		
 		time.sleep(.5)
 	except IOError:
 		print('Error')
